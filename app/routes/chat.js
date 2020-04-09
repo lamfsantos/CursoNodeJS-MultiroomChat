4d868a6fa4e2d541a76a8c00dce8c37498d42276
Furlan <lamfsantos5@gmail.com>
2020-04-08 21:46:14 -0400

    Ajuste das rotas e adição das views estáticas

---
+++ app/routes/chat.js
@@ -0,0 +1,8 @@
+module.exports = function(application) {
+	application.post('/chat', function(request, response){
+		response.render("chat")
+	})
+	application.get('/chat', function(request, response){
+		response.render("chat")
+	})
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 module.exports = function(application) {
 	application.get('/', function(request, response){
-		response.send('Primeira config funcionando')
+		response.render("index")
 	})
 }
